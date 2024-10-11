c4d830d753e323c72937d105cc31ec275fb0b646
Svenebanan1 <Casper.aaroe@hotmail.com>
2024-10-11 13:49:19 +0200

    AAAA

---
+++ Space invaders/Assets/Scripts/Missile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Missile : Projectile
+{
+    private void Awake()
+    {
+        direction = Vector3.down;
+    }
+
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject); //s� fort den krockar med n�got s� ska den f�rsvinna.
+    }
+
+}
---
+++ Space invaders/Assets/Scripts/MysteryShip.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MysteryShip : MonoBehaviour
+{
+    float speed = 5f;
+    float cycleTime = 5f;
+
+    Vector2 leftDestination;
+    Vector2 rightDestination;
+    int direction = -1;
+    bool isVisible;
+
+
+    void Start()
+    {
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        //positionen d�r den kommer stanna utanf�r sk�rmen.
+        leftDestination = new Vector2(leftEdge.x - 1f, transform.position.y);
+        rightDestination = new Vector2(rightEdge.x + 1f, transform.position.y);
+
+        SetInvisible();
+    }
+
+
+    void Update()
+    {
+        if (!isVisible) //�r den inte synlig s� ska den ej r�ra sig.
+        {
+            return;
+        }
+
+        if (direction == 1)
+        {
+            //r�r sig �t h�ger
+            transform.position += speed * Time.deltaTime * Vector3.right;
+
+            if (transform.position.x >= rightDestination.x)
+            {
+                SetInvisible();
+            }
+        }
+        else
+        {
+            //r�r sig �t v�nster
+            transform.position += speed * Time.deltaTime * Vector3.left;
+
+            if (transform.position.x <= leftDestination.x)
+            {
+                SetInvisible();
+            }
+        }
+    }
+
+
+    //flyttar den till en plast precis utanf�r scenen.
+    void SetInvisible()
+    {
+        isVisible = false;
+
+        if (direction == 1)
+        {
+            transform.position = rightDestination;
+        }
+        else
+        {
+            transform.position = leftDestination;
+        }
+
+        Invoke(nameof(SetVisible), cycleTime); //anropar SetVisible efter ett visst antal sekunder
+    }
+
+    void SetVisible()
+    {
+        direction *= -1; //�ndrar riktningen
+
+        isVisible = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Laser"))
+        {
+            SetInvisible();
+            GameManager.Instance.OnMysteryShipKilled(this);
+        }
+    }
+}
