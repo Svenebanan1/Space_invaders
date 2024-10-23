3f8e8469121cdf7af15a0f532ccad951e4da5998
Svenebanan1 <Casper.aaroe@hotmail.com>
2024-10-23 09:37:56 +0200

    vvw

---
+++ Space invaders/Assets/Scripts/backgroundmain.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class backgroundmain : Projectile
+{
+    private Vector2 Position;
+    float Speed = 7f;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Speed * Time.deltaTime * direction;
+
+        Position = transform.position;
+
+        direction = Vector3.down*speed;
+
+        if(Position.y <= -36)
+        {
+            
+            transform.position = new Vector2(0, 36);
+        }
+       
+
+    }
+}
